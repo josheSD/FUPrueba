e4132a330b228fe306fce11fb788ffa2da395717
Jose Chutas Ramos <jchutas@dcp.pe>
2021-04-26 13:07:42 -0500

    init app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,58 @@
-import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// Envinronment
+import { environment } from './../environments/environment';
+
+// Shared
+import { AuthModule } from './shared/layouts/auth/auth.module';
+import { AdminModule } from './shared/layouts/admin/admin.module';
+import { SplashScreenModule } from './shared/splash-screen/splash-screen.module';
+import { NotFoundPageModule } from './shared/not-found-page/not-found-page.module';
+
+// Ngrx
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { appReducers } from './store/reducers/app.reducer';
+
+// Services
+import { ConfigService, LoadConfiguration } from '@core/services/config/config.service';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    SplashScreenModule,
+    NotFoundPageModule,
+    AdminModule,
+    AuthModule,
+    StoreModule.forRoot(appReducers),
+    EffectsModule.forRoot([]),
+    StoreRouterConnectingModule.forRoot({stateKey: 'router'}),
+    StoreDevtoolsModule.instrument({
+      maxAge:25,
+      logOnly: environment.production
+    })
   ],
-  providers: [],
+  providers: [
+    ConfigService,
+    {
+      provide: APP_INITIALIZER,
+      useFactory: LoadConfiguration,
+      deps:[ConfigService],
+      multi:true
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
